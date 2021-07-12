b0afcc53a2a58c0a8b5f88b61075afb31611cbca
Maneesh P <maneesh@Maneeshs-MacBook-Pro.local>
2021-07-12 22:38:37 +0530

    mobile view full height fixes

--- src/home/index.js
+++ src/home/index.js
@@ -7,8 +7,18 @@ import Dashboard from '../components/Dashboard'
 const Home = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [locationId, setLocationId] = useState(44418)
+
   const { loading: locLoading, error: locationError, locations } = useGetLocations(searchTerm);
   const { loading: weatherLoading, error: weatherError, weatherData } = useGetWeatherData(locationId)
+
+  window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  
   return (
     <div className="home--container">
       <Sidebar
